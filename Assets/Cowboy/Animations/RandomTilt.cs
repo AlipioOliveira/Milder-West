42ef78883c13f733f906a819adb6a31fa2a6f7ae
Hey ಠ  ͜  ಠ <alipio-luis@hotmail.com>
2017-04-30 18:23:43 +0100

    tentativa de minigame

---
+++ Assets/Cowboy/Animations/RandomTilt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTilt : MonoBehaviour {
+    Animator animator;
+    float timer;
+    int i;
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        timer = animator.GetCurrentAnimatorStateInfo(0).length;
+    }
+    
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            i = Random.Range(0, 4);
+            animator.SetInteger("Anim", i);            
+            SetTimer();
+        }
+    }
+
+    private void SetTimer()
+    {
+        timer = animator.GetCurrentAnimatorStateInfo(0).length;
+    }
+}
